a85ba7475e23bb1965800aa1895f2556e3c00623
rebaz36 <rebaz415@gmail.com>
2022-10-31 22:21:41 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 // component
-// import TodoContainer from './functionBased/components/TodoContainer';
+import TodoContainer from './functionBased/components/TodoContainer';
 
 // stylesheet
 import './functionBased/App.css';
@@ -12,7 +12,7 @@ import './functionBased/App.css';
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
-      {/* <TodoContainer /> */}
+      <TodoContainer />
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
